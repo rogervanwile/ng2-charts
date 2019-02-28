1faf3bddce2c5590dfc9091683874d86c32188fd
Oliver Wagner <oliver.h.wagner@googlemail.com>
2019-02-28 11:59:21 +0100

    Support for hidden-Attribute on dataset changes

--- src/charts/charts.ts
+++ src/charts/charts.ts
@@ -131,6 +131,7 @@ export class BaseChartDirective implements OnDestroy, OnChanges, OnInit {
         if (newDataValues[i].label) {
           dataset.label = newDataValues[i].label;
         }
+        dataset.hidden = newDataValues[i].hidden;
       });
     } else {
       this.chart.data.datasets[0].data = newDataValues;
